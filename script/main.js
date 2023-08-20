611b09510bbc0d2397d8fbc4819540a019a60a42
solugionmaker99 <frriyad111@gmail.com>
2023-08-21 00:35:11 +0600

    last night commit

---
+++ script/main.js
@@ -0,0 +1,140 @@
+
+// Card 1
+prod1.addEventListener('click', ()=>{
+
+    const prod1 = document.getElementById('prod1');
+    const cardName= prod1.querySelector('.card-title').textContent;
+    const cardPriceString = prod1.querySelector('.price').textContent;
+    const cardPrice = parseInt(cardPriceString);
+
+    purchesItems(cardName, cardPrice)
+})
+
+// Card 2
+prod2.addEventListener('click', ()=>{
+
+    const prod2 = document.getElementById('prod2');
+    const cardName= prod2.querySelector('.card-title').textContent;
+    const cardPriceString = prod2.querySelector('.price').textContent;
+    const cardPrice = parseInt(cardPriceString);
+
+    purchesItems(cardName, cardPrice)
+})
+
+// card 3
+prod3.addEventListener('click', ()=>{
+
+    const prod3 = document.getElementById('prod3');
+    const cardName= prod3.querySelector('.card-title').textContent;
+    const cardPriceString = prod3.querySelector('.price').textContent;
+    const cardPrice = parseInt(cardPriceString);
+
+    purchesItems(cardName, cardPrice)
+})
+
+// card 4
+prod4.addEventListener('click', ()=>{
+
+    const prod4 = document.getElementById('prod4');
+    const cardName= prod4.querySelector('.card-title').textContent;
+    const cardPriceString = prod4.querySelector('.price').textContent;
+    const cardPrice = parseInt(cardPriceString);
+
+    purchesItems(cardName, cardPrice)
+})
+
+// card 5
+prod5.addEventListener('click', ()=>{
+
+    const prod5 = document.getElementById('prod5');
+    const cardName= prod5.querySelector('.card-title').textContent;
+    const cardPriceString = prod5.querySelector('.price').textContent;
+    const cardPrice = parseInt(cardPriceString);
+
+    purchesItems(cardName, cardPrice)
+})
+
+// card 6
+prod6.addEventListener('click', ()=>{
+
+    const prod6 = document.getElementById('prod6');
+    const cardName= prod6.querySelector('.card-title').textContent;
+    const cardPriceString = prod6.querySelector('.price').textContent;
+    const cardPrice = parseInt(cardPriceString);
+
+    purchesItems(cardName, cardPrice)
+})
+
+// card 7
+prod7.addEventListener('click', ()=>{
+
+    const prod7 = document.getElementById('prod7');
+    const cardName= prod7.querySelector('.card-title').textContent;
+    const cardPriceString = prod7.querySelector('.price').textContent;
+    const cardPrice = parseInt(cardPriceString);
+
+    purchesItems(cardName, cardPrice)
+}) 
+
+// card 8
+prod8.addEventListener('click', ()=>{
+
+    const prod8 = document.getElementById('prod8');
+    const cardName= prod8.querySelector('.card-title').textContent;
+    const cardPriceString = prod8.querySelector('.price').textContent;
+    const cardPrice = parseInt(cardPriceString);
+
+    purchesItems(cardName, cardPrice)
+})
+
+// card 9
+prod9.addEventListener('click', ()=>{
+
+    const prod9 = document.getElementById('prod9');
+    const cardName= prod9.querySelector('.card-title').textContent;
+    const cardPriceString = prod9.querySelector('.price').textContent;
+    const cardPrice = parseInt(cardPriceString);
+
+    purchesItems(cardName, cardPrice)
+})
+
+purchesItems = (cardName, cardPrice) => {
+    console.log(cardName + " " + cardPrice);
+    const productList = document.getElementById('product-list');
+    const p = document.createElement('p');
+    p.innerHTML = cardName;
+    // p.classList.add("text-[#111] text-2xl font-medium leading-[48px] mb-6")
+    productList.appendChild(p);
+
+    const totalPriceString = document.getElementById('total-price');
+
+
+    const totalPrice = totalPriceString.innerText;
+    const totalPriceNumber = parseInt(totalPrice);
+    const value = totalPriceNumber + cardPrice;
+
+    totalPriceString.innerText = value + " " + "TK";
+
+    const discount = document.getElementById('discount');
+
+    // const totalString = document.getElementById('total').innerText;
+    // const totalValue = parseInt(totalString);
+
+    // const couponId = document.getElementById('coupon-id');
+    // const disabled = true;
+
+    // console.log("Total Value", totalValue)
+   
+   
+
+}
+
+
+
+
+
+
+
+
+
+
